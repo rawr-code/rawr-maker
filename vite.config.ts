d9466c8efe979cbbc1c76dbef2da752e707bcaf2
RawrCode <rawr.code.rawr@gmail.com>
2023-12-17 14:49:47 -0400

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Do not inline images and assets to avoid the phaser error
+    // "Local data URIs are not supported"
+    assetsInlineLimit: 0,
+  },
+});
